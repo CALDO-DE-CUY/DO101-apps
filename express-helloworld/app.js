cfc1312a856c236f25dde22f05ccd4314787cdac
CALDO-DE-CUY <da.barraza24@info.uas.edu.mx>
2025-05-30 03:57:06 -0400

    update to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars, Diego Alejandro Barraza Chavez!\n');
+});
+
+app.get('/pluton', function(req, res) {
+  res.send('Hello Pluton, Diego Alejandro Barraza Chavez!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
